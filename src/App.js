431f5b1449556bab1f9eb274eacdc8e435966bd0
Manuareraa <manuareraa@gmail.com>
2022-12-03 20:27:06 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,108 @@
-import React from 'react';
-import './App.css';
-import BlockNav from './Components/Block1'
-import Block2 from './Components/Block2/Block2';
-import Platform from './Components/Block3/Platform';
-import Footer from './Components/Footer/Footer';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import BlockNav from "./Components/Block1";
+import Block2 from "./Components/Block2/Block2";
+import Platform from "./Components/Block3/Platform";
+import Footer from "./Components/Footer/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Web3 from "web3";
 
-function LandingPage(){
-  return(
+function LandingPage(props) {
+  return (
     <div className="landingPage ">
-      <BlockNav />
-      <Block2/>
-      <Platform/>
+      <BlockNav
+        ConnectWallet={props.ConnectWallet}
+        appState={props.appState}
+        setAppState={props.setAppState}
+      />
+      <Block2 />
+      <Platform />
       <Footer />
     </div>
-  )
+  );
 }
 
 function App() {
+  const [appState, setAppState] = useState({
+    isLoggedIn: false,
+    account: "",
+    isMetamask: true,
+  });
+
+  const ConnectWallet = () => {
+    console.log("Connect Wallet", window.ethereum);
+    if (window.ethereum) {
+      appState.isWeb3 = true;
+      const ethereum = window.ethereum;
+      let web3 = new Web3(ethereum);
+      ethereum.enable().then((accounts) => {
+        let account = accounts[0];
+        web3.eth.defaultAccount = account;
+        web3.eth.transactionConfirmationBlocks = 10;
+        console.log("Dets", account, appState, ethereum.chainId);
+        setAppState((prevState) => {
+          return {
+            ...prevState,
+            isLoggedIn: true,
+            // account: web3.utils.toChecksumAddress(accounts[0]),
+            account: accounts[0],
+            web3: web3,
+          };
+        });
+        switchNetworkMumbai(web3, ethereum, accounts[0]);
+      });
+    } else {
+      setAppState((prevState) => {
+        return { ...prevState, isMetamask: false };
+      });
+    }
+  };
+
+  const switchNetworkMumbai = async (web3, ethereum, account) => {
+    try {
+      await ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x13881" }],
+      });
+    } catch (switchError) {
+      // This error code indicates that the chain has not been added to MetaMask.
+      if (switchError.code === 4902) {
+        try {
+          await ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: "0x61",
+                chainName: "Polygon Mumbai Testnet",
+                rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
+              },
+            ],
+          });
+        } catch (addError) {
+          console.error("Error while adding new chain to MetaMask");
+        }
+      }
+      // handle other "switch" errors
+    }
+  };
+
   return (
     <div className="App ">
-        <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/"
+            element={
+              <LandingPage
+                ConnectWallet={ConnectWallet}
+                appState={appState}
+                setAppState={setAppState}
+              />
+            }
+          />
         </Routes>
-        </BrowserRouter>
-  
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Block1.jsx
+++ src/Components/Block1.jsx
@@ -1,118 +1,109 @@
-import React from 'react';
-import './Block.scss'
-import { CardsData } from '../Data/cardsData'
+import React from "react";
+import "./Block.scss";
+import { CardsData } from "../Data/cardsData";
 // import Web3 from "web3";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function BlockNav() {
-    //     const [address, setAddress] = useState([]);
+function BlockNav(props) {
+  //     const [address, setAddress] = useState([]);
 
-    //   async function requiredWallet() {
-    //     const web = new Web3(window.ethereum);
+  //   async function requiredWallet() {
+  //     const web = new Web3(window.ethereum);
 
-    //     if (web) {
-    //       console.log("detected");
-    //       try {
-    //         const accounts = await window.ethereum.request({
-    //           method: "eth_requestAccounts"
-    //         });
-    //         setAddress(accounts);
-    //       } catch (error) {
-    //         console.log(error);
-    //       }
-    //     } else {
-    //       console.log("metamak need");
-    //     }
-    //   }
-    return (
+  //     if (web) {
+  //       console.log("detected");
+  //       try {
+  //         const accounts = await window.ethereum.request({
+  //           method: "eth_requestAccounts"
+  //         });
+  //         setAddress(accounts);
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     } else {
+  //       console.log("metamak need");
+  //     }
+  //   }
+  return (
+    <div className="relative">
+      <div className="block-content">
+        <div className="container mx-auto flex flex-row justify-between items-center p-4 ">
+          <div className="logo basis-1/4">
+            <h3 className="text-pink ">_Staple</h3>
+          </div>
 
-        <div className='relative'>
+          {props.appState.isLoggedIn === true ? (
+            <>
+              <div>
+                <ul>
+                  <li>Home</li>
+                  <li>Write</li>
+                  <li>Explore</li>
+                  <li>staple</li>
+                </ul>
+              </div>
+            </>
+          ) : null}
+          <div className="menus flex space-x-6">
+            <div className="hidden md:flex space-x-6 items-center">
+              <Link to="#">How we build</Link>
 
-            <div className='block-content'>
-
-                <div className="container mx-auto flex flex-row justify-between items-center p-4 ">
-                    <div className='logo basis-1/4'>
-                        <h3 className='text-pink '>_Staple</h3>
-                    </div>
-                    {/* {
-                    address ? (
-                        null
-                    ):(<div>
-                            <ul>
-                                <li>Home</li>
-                                <li>Write</li>
-                                <li>Explore</li>
-                                <li>staple</li>
-                             
-                            </ul>
-                        </div>)
-                } */}
-
-                    <div className='menus flex space-x-6'>
-                        <div className='hidden md:flex space-x-6 items-center'>
-                            <Link to="#">
-                                How we build
-                            </Link>
-                            
-                            <Link to="#">
-                                Refer & Earn
-                            </Link>
-                        </div>
-                        <button className='bg-orange-400 p-2 w-25 h-10 rounded-sm'
-                        //  onClick={requiredWallet}
-                        >
-                            Connect Wallet
-                        </button>
-
-                    </div>
-                </div>
-                <div className='w-3/4 text-center m-0 m-auto mt-16' >
-                    <h1 className='font-bold text-4xl  leading-normal mb-5'>
-                        Organize all your content<br /> in one place
-                    </h1>
-                    <p className='leading-normal text-lg mb-4'>Building a gatherer economy for creators & <br /> consumers on Web3</p>
-                    <button className='bg-orange-400 p-3 rounded-sm w-64 mb-3'>Early Bird Signup</button><br />
-                    <span className='text-md '>Get 100 Staple Tokens on Launch*</span>
-                </div>
-
-
+              <Link to="#">Refer & Earn</Link>
             </div>
-
-            <CardsBlock />
+            <button
+              className="bg-orange-400 p-2 w-25 h-10 rounded-sm"
+              onClick={props.ConnectWallet}
+            >
+              {props.appState.account !== "" ? (
+                <>{props.appState.account.slice(0,4) + ".." + props.appState.account.slice(-4)}</>
+              ) : (
+                <>Connect Wallet</>
+              )}
+            </button>
+          </div>
         </div>
+        <div className="w-3/4 text-center m-0 m-auto mt-16">
+          <h1 className="font-bold text-4xl  leading-normal mb-5">
+            Organize all your content
+            <br /> in one place
+          </h1>
+          <p className="leading-normal text-lg mb-4">
+            Building a gatherer economy for creators & <br /> consumers on Web3
+          </p>
+          <button className="bg-orange-400 p-3 rounded-sm w-64 mb-3">
+            Early Bird Signup
+          </button>
+          <br />
+          <span className="text-md ">Get 100 Staple Tokens on Launch*</span>
+        </div>
+      </div>
 
-    )
+      <CardsBlock />
+    </div>
+  );
 }
-
 
 const CardsBlock = () => {
-    return (
-        <div className='crds-blk flex'>
-            <div className='crds-blk shadow-lg shadow-indigo-500/40  xl:w-[1000px] md:w-[1000px]  m-0 m-auto h-46 bg-white rounded-md p-6 flex flex-col  xl:flex-row xl:space-x-6 md:flex-row md:space-x-6 '>
-                {
-                    CardsData.map((items) => {
-                        return (
-                            <div className='crds flex-auto flex flex-row'>
-                                <div className='flex-none w-14  bg-orange-300 h-14 rounded-full p-3'>
-                                    <img src={items.img} alt="" />
-                                </div>
-                                &nbsp;&nbsp;&nbsp;
-                                <div className='flex-auto'>
-                                    <h1 className='font-bold leading-10'>
-                                        {items.name}
-                                    </h1>
-                                    <p>{items.desc}</p>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+  return (
+    <div className="crds-blk flex">
+      <div className="crds-blk shadow-lg shadow-indigo-500/40  xl:w-[1000px] md:w-[1000px]  m-0 m-auto h-46 bg-white rounded-md p-6 flex flex-col  xl:flex-row xl:space-x-6 md:flex-row md:space-x-6 ">
+        {CardsData.map((items) => {
+          return (
+            <div className="crds flex-auto flex flex-row">
+              <div className="flex-none w-14  bg-orange-300 h-14 rounded-full p-3">
+                <img src={items.img} alt="" />
+              </div>
+              &nbsp;&nbsp;&nbsp;
+              <div className="flex-auto">
+                <h1 className="font-bold leading-10">{items.name}</h1>
+                <p>{items.desc}</p>
+              </div>
             </div>
-        </div>
-    )
-}
-
-
-
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default BlockNav;
